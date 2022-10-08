03e15e6a7b489553ab0dd5a96c0ba528d85d27fe
Farah M <farah.maria.rahman@gmail.com>
2022-10-08 23:51:44 +0000

    commit2

--- woof.test.js
+++ woof.test.js
@@ -3,5 +3,5 @@ const woof = require("./woof");
 test("should return number of woofs", function() {
    const result = woof("oh herro");
    expect(result).toBe("8woof!"); //strict equality
-   expect(result).toEqual("8woof!"); //not strict equality
+   expect(['a', 'b', 'c']).toContain('b'); 
 });
